70208e2700b23cf124db9d6a88a0f226bacc4ad7
van nguyen <vannguyengithub@gmail.com>
2023-12-10 20:11:16 +0700

    filter status

--- routes/backend/items.js
+++ routes/backend/items.js
@@ -2,16 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 const ItemsModel = require('./../../schemas/items');
+const UtilsHelpers = require('./../../helpers/utils');
 
 
 
 router.get('/',  (req, res, next) => {
+    let statusFilter = UtilsHelpers.createFilterStatus();
+    
     ItemsModel.find({ }).then( (items ) => {
         res.render('pages/items/list', { 
             pageTitle: 'Items',
-            items: items 
+            items: items,
+            statusFilter: statusFilter
         });
-    })
+    });
 
 });
 
