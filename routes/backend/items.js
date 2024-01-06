18eda5537bb6dd8b283983dea457afd353a2c2a3
van nguyen <vannguyengithub@gmail.com>
2024-01-06 23:22:58 +0700

    up

--- routes/backend/items.js
+++ routes/backend/items.js
@@ -7,30 +7,46 @@ const ParamsHelpers = require('./../../helpers/params');
 
 
 
-router.get('(/:status)?',  (req, res, next) => {
+router.get('(/:status)?',  async (req, res, next) => {
     let objWhere = {}
     let keyword = ParamsHelpers.getParam(req.query, 'keyword', '');
-
     let currentStatus = ParamsHelpers.getParam(req.params, 'status', 'all');
-    
     let statusFilter = UtilsHelpers.createFilterStatus(currentStatus);
     
+    let pagination = {
+        totalItem           : 1,  
+        totalItemPerPage    : 2,
+        currentPage         : 1,
+        pageRanges          : 3,
+    };
+
+    pagination.currentPage = parseInt(ParamsHelpers.getParam(req.query, 'page', 1));
+
     if(currentStatus === 'all') {
         if(keyword !== '') objWhere = { name: new RegExp(keyword, 'i') };
     } else {
         objWhere = {status: currentStatus, name: new RegExp(keyword, 'i') };
     }
 
+    // ItemsModel.countDocuments(objWhere).then( (data) => {
+    //     pagination.totalItem = data;
+    // });
+    const data = await ItemsModel.countDocuments(objWhere);
+    pagination.totalItem = data;
+
     ItemsModel
         .find(objWhere)
         .sort({ordering: 'asc'})
+        .skip((pagination.currentPage - 1) * pagination.totalItemPerPage)
+        .limit(pagination.totalItemPerPage)
         .then( (items ) => {
         res.render('pages/items/list', { 
             pageTitle: 'Items',
             items: items,
             statusFilter: statusFilter,
             currentStatus: currentStatus,
-            keyword: keyword
+            keyword: keyword,
+            pagination: pagination
         });
     });
 });
